d2eb4f7aa66a78ebb19512d55237fd3f7ce0261e
Tommy Yu <tommy.yu@auckland.ac.nz>
2016-09-06 13:25:47 +1200

    Adding CI
    - Also renamed test_suite to make_suite.

--- src/calmjs/rjs/tests/__init__.py
+++ src/calmjs/rjs/tests/__init__.py
@@ -2,13 +2,10 @@ import unittest
 from os.path import dirname
 
 
-def test_suite():
-    import calmjs.rjs
+def make_suite():  # pragma: no cover
     test_loader = unittest.TestLoader()
     test_suite = test_loader.discover(
         'calmjs.rjs.tests', pattern='test_*.py',
-        # namespace packages are actually going to interfere if not
-        # very explicit here.
-        top_level_dir=dirname(calmjs.rjs.__file__)
+        top_level_dir=dirname(__file__)
     )
     return test_suite
