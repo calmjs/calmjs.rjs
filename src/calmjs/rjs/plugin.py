d49f4328817eb3144a848da8c19934992c10d10e
Tommy Yu <tommy.yu@auckland.ac.nz>
2016-09-27 15:50:45 +1300

    Provide system for handling requirejs plugins
    - So the deal with module names and paths and everything about the names
      provided and used by the loader plugin implementations are proprietary
      to each and every one of them, thus the names and paths required will
      varies very wildly between all of them.
    - At the very least, provide a simple implementation for a commonly used
      implementation.

---
+++ src/calmjs/rjs/plugin.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+The plugin system compatibility layer for requirejs.
+
+Given that the plugin definitions are done on top of the module system,
+its declarations are on the same level however each plugin has the
+potential to implement their own syntax system, thus the resource name
+referenced can be anything.  Thus this module is needed to normalize the
+provided "modname" entries into the ones that are understood here.  The
+functions must have the following signature
+
+    def plugin(toolchain, spec, modname, source, target, modpath):
+        # do processing
+        # typical return value
+        return {modname: modpath}, [modname]
+
+The RJSToolchain compile_plugins method will make use of functions of
+the above type registered to the registry system.
+"""
+
+import shutil
+from os import makedirs
+from os.path import dirname
+from os.path import exists
+from os.path import join
+
+
+def text(toolchain, spec, modname, source, target, modpath):
+    """
+    Each of these should return this bundled_path and the exported
+    module_name
+    """
+
+    plugin_name, resource_name = modname.split('!', 1)
+    target = resource_name
+    copy_target = join(spec['build_dir'], target)
+    if not exists(dirname(copy_target)):
+        makedirs(dirname(copy_target))
+    shutil.copy(source, copy_target)
+    bundled_paths = {modname: target}
+    module_names = [modname]
+    return bundled_paths, module_names
