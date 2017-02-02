62e204d247edd96ec88c63b25c5ac1bfb22a59f3
Tommy Yu <tommy.yu@auckland.ac.nz>
2017-02-02 15:10:33 +1300

    CI Version bumps due to release of Python 3.6

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
 """.strip().splitlines()
 
 package_json = {
